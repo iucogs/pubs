cffd4e6d73dc0456957836a049c273e61804cfc5
Patrick Craig <pjcraig@indiana.edu>
2012-08-28 15:41:03 -0400

    This test is so nice I did it thrice

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once($document_root.'lib/adLDAP.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" ></meta>
-<title>Publications <?php if(DB_NAME != "pubs") echo " [".DB_NAME."]";?></title>
+<title>Publications currently on <?php if(DB_NAME != "pubs") echo " [".DB_NAME."]";?></title>
 
 <?php
 // Variable declarations
