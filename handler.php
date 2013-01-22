e258dbc8b89e10c3edae29b83bf5874af0a7d4c5
Patrick Craig <pjcraig@indiana.edu>
2013-01-22 14:32:32 -0500

    Function stubs, rewrote documentation, refactored handling

--- handler.php
+++ handler.php
@@ -1,5 +1,12 @@
 <?php 
 
+/************************
+ IU COGS PUBS API Handler
+ Written by: Patrick Craig/pjcraig
+ Maintained by:
+ Refactored Jan 22, 2013
+ ************************/
+
 require_once('/home/patrick/Sites/pubs/classes/Citations.class.php');
 require_once('/home/patrick/Sites/pubs/classes/Collections.class.php');
  
@@ -8,50 +15,73 @@ $collections = new Collections();
 $path = $_SERVER['REQUEST_URI'];
 $raw = $_POST['raw'];
 
-// Takes a raw citation string sent via POST and returns 
-// a JSON object containing the citation info
-function parse( $raw ) {
+/*************************
+ POST /citation/parse
+ input: raw citation string
+ output: JSON string containing citation info
+ *************************/
+function parse_POST($raw) {
     
 }
 
-// Takes a raw citation string, calls parse, saves the 
-// JSON returned by parse to the database and returns
-// the new ID of the citation.
-function citation( $raw ) {
+/*************************
+ POST /citation
+ input: raw citation string
+ output: ID of newly created citation
+ *************************/
+function citation_POST($raw) {
 
 }    
 
-// Takes a raw citation string, and with fuzzymatch
-// returns a JSON array of potentially matching citations.
-function match ( $raw ) {
+/*************************
+ POST /citation/match/
+ input: raw citation string
+ output: JSON of potentially matching citations
+ *************************/
+function match_POST($raw) {
 
 }
 
-// Takes the path, substrings the citation ID out, writes it to a file.
-function citation_handler( $path ) {
+/*************************
+ GET /citation/{citationnumber.json}
+ input: Number of an existing citation with .json appended
+ output: JSON of the citation's entry in the database
+ *************************/
+function citation_GET($path) {
     global $citations;
-    $citation_id = substr( $path, 10, -5 );	
+    $citation_id = substr($path, 10, -5);	
     $citation_json = json_encode($citations->getCitation_byID2($citation_id)); 
 	echo $citation_json;
 }
 
-// Pretty much the exact same thing
-
-function collection_handler( $path ) {
+/*************************
+ GET /citation/{citationnumber.json}
+ input: Number of an existing citation with .json appended
+ output: JSON of the citation's entry in the database
+ *************************/
+function collection_GET($path) {
     global $collections;
-    $collection_id = substr( $path, 12, -5 ); 
+    $collection_id = substr($path, 12, -5); 
     $collection_json = json_encode($collections->getCollectionByID($collection_id));
 	echo $collection_json;
 }
 
- 
-if ( strpos( $path, 'citation' ) != false )
-    citation_handler($path); 
-else if ( strpos( $path, 'collection' ) != false )
-    collection_handler($path);
-else 
-    ;
+// Actual handling happens here.
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
+  if (strpos($path, 'citation') != false)
+      citation_GET($path); 
+  else if (strpos($path, 'collection' ) != false)
+      collection_GET($path);
+  else 
+      ;
+} else {
+  if (strpos($path, 'parse') != false) 
+      parse($_POST['raw']);
+  else if (strpos($path, 'match') != false)
+      match($_POST['raw']);
+  else
+      citation_post($_POST['raw']);
 
 
 
