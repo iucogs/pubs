e8e87d7b5143de5c2cdafe4912fff8486380846f
Patrick Craig <pjcraig@indiana.edu>
2013-03-04 15:12:21 -0500

    experimental controller script for compartmentalized handlers

--- boss.php
+++ boss.php
@@ -10,32 +10,70 @@
  ****************************/
 
 // Variables
-$citations = Array();
-$IDs = Array();
-$method;
-$result;
-$resultAccumulator = Array();
-$cmd = "php handler.php ";
-$redirect = " > /dev/null 2>&1 &";
-$debug;
+$cmd = "php ";              
+$debug;                      // Debug flag
+$handler;                    // Tells us which handler to use
+$function;                   // Function to call in the respective handler
+$resultAccumulator = Array();          
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $method = "POST ".$_POST['method']." ";
-  $citations = explode("\n", $_POST['citations']);
-  foreach ($citations as $citation) {
-    array_push($resultAccumulator, exec($cmd.$method.'"'.$citation.'"'));  
-   } 
-  } else {
-  if (strpos($_SERVER['REQUEST_URI'], "citation") != false)
-    $method = "GET citation ";
-  else
-    $method = "GET collection ";
-  
-  $IDs = explode(",", $_GET['IDs']);
-  foreach ($IDs as $ID) {
-    array_push($resultAccumulator, exec($cmd.$method.$ID));
-  }
+// Detects citations request vs collections request
+if (strpos($_SERVER['REQUEST_URI'], 'citation'))
+  $cmd .= "citation_handler.php ";
+else 
+  $cmd .= "collection_handler.php ";
+
+// Build exec command, execute, populate resultAccumulator
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'POST': 
+    $cmd .= $_POST['function']." ";
+    
+    // We either are posting citations or collections
+    if (strlen($_POST['citations'] > 0)) {
+      $citations = explode("\n", $_POST['citations']);
+      foreach ($citations as $citation) {
+        array_push($resultAccumulator, exec($cmd.'"'.$citation.'"'));  
+      } 
+    } else {
+      // Separate out data
+      $collectionNames = explode("\n", $_POST['collectionNames']);
+      $submitters = explode("\n", $_POST['submitters']);
+      $owners = explode("\n", $_POST['owners']);
+      
+      for ($i = 0; $i < sizeof($collectionNames); $i++) { 
+        array_push($resultAccumulator, exec($cmd.'"'.$collectionNames[$i].'" "'.$submitters[$i].'" "'.$owners[$i].'"'));
+      }  
+    }
+ 
+    break;
+
+  // for now we're just adding citations to collection
+  case 'PUT':
+    $putVars = parse_str(file_get_contents("php://input"),$putVars);
+    $cmd .= $putVars['function']." ";
+    $collectionID =  $putVars['collectionID'];
+    $citationIDs = explode(',', $putVars['citationIDs']);
+
+    for ($i = 0; $i < sizeof($citationIDs); $i++) {
+      array_push($resultAccumulator, exec($cmd.'"'.$collectionID.'" "'.$citationIDs[$i].'"'));  
+    }
+    
+    break;
+
+  case 'GET':
+    if (strpos($cmd, 'citation')) 
+        $cmd .= 'citation_GET ';
+    else
+        $cmd .= 'collection_GET ';
+
+    $IDs = explode(",", $_GET['IDs']);
+    foreach ($IDs as $ID) { 
+      array_push($resultAccumulator, exec($cmd.'"'.$ID.'"'));
+    }
+    break;
 }
+var_dump($IDs);
+echo $cmd;
+var_dump($resultAccumulator);
 echo stripslashes(json_encode($resultAccumulator));
 ?>
